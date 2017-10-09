1be1ae0b3bbcaa71f135d5677ad82d0c434fbd2a
Shaohua Dong <32624425+dsh940125@users.noreply.github.com>
2017-10-09 11:57:50 -0400

    Update and rename Assignment+4.py to Random Forest for Property Maintenance Fines.py

--- Random Forest for Property Maintenance Fines.py
+++ Random Forest for Property Maintenance Fines.py
@@ -1,54 +1,20 @@
 
 # coding: utf-8
 
+ 
+# This project is based on a data challenge from the Michigan Data Science Team, which have partnered with the City of Detroit 
+# to help solve the blight problem. The problem is issued by the city to individuals who allow their properties to remain in a 
+# deteriorated condition. Every year, the city of Detroit issues millions of dollars in fines to residents and every year. Many 
+# of these fines remain unpaid. Enforcing unpaid blight fines is a costly and tedious process, so the city wants to know how 
+# can we increase blight ticket compliance? My task is to build a model ith an AUROC (the Area Under the ROC Curve) of 0.7 to 
+# predict whether a given blight ticket will be paid on time.
 
-# ## Assignment 4 - Understanding and Predicting Property Maintenance Fines
-# 
-# This assignment is based on a data challenge from the Michigan Data Science Team ([MDST](http://midas.umich.edu/mdst/)). 
-# 
-# The Michigan Data Science Team ([MDST](http://midas.umich.edu/mdst/)) and the Michigan Student Symposium for Interdisciplinary Statistical Sciences ([MSSISS](https://sites.lsa.umich.edu/mssiss/)) have partnered with the City of Detroit to help solve one of the most pressing problems facing Detroit - blight. [Blight violations](http://www.detroitmi.gov/How-Do-I/Report/Blight-Complaint-FAQs) are issued by the city to individuals who allow their properties to remain in a deteriorated condition. Every year, the city of Detroit issues millions of dollars in fines to residents and every year, many of these fines remain unpaid. Enforcing unpaid blight fines is a costly and tedious process, so the city wants to know: how can we increase blight ticket compliance?
-# 
-# The first step in answering this question is understanding when and why a resident might fail to comply with a blight ticket. This is where predictive modeling comes in. For this assignment, your task is to predict whether a given blight ticket will be paid on time.
-# 
-# All data for this assignment has been provided to us through the [Detroit Open Data Portal](https://data.detroitmi.gov/). **Only the data already included in your Coursera directory can be used for training the model for this assignment.** Nonetheless, we encourage you to look into data from other Detroit datasets to help inform feature creation and model selection. We recommend taking a look at the following related datasets:
-# 
-# * [Building Permits](https://data.detroitmi.gov/Property-Parcels/Building-Permits/xw2a-a7tf)
-# * [Trades Permits](https://data.detroitmi.gov/Property-Parcels/Trades-Permits/635b-dsgv)
-# * [Improve Detroit: Submitted Issues](https://data.detroitmi.gov/Government/Improve-Detroit-Submitted-Issues/fwz3-w3yn)
-# * [DPD: Citizen Complaints](https://data.detroitmi.gov/Public-Safety/DPD-Citizen-Complaints-2016/kahe-efs3)
-# * [Parcel Map](https://data.detroitmi.gov/Property-Parcels/Parcel-Map/fxkw-udwf)
-# 
-# ___
-# 
-# We provide you with two data files for use in training and validating your models: train.csv and test.csv. Each row in these two files corresponds to a single blight ticket, and includes information about when, why, and to whom each ticket was issued. The target variable is compliance, which is True if the ticket was paid early, on time, or within one month of the hearing data, False if the ticket was paid after the hearing date or not at all, and Null if the violator was found not responsible. Compliance, as well as a handful of other variables that will not be available at test-time, are only included in train.csv.
-# 
-# Note: All tickets where the violators were found not responsible are not considered during evaluation. They are included in the training set as an additional source of data for visualization, and to enable unsupervised and semi-supervised approaches. However, they are not included in the test set.
-# 
-# <br>
-# 
-# **File descriptions** (Use only this data for training your model!)
+
+# File descriptions
 # 
 #     train.csv - the training set (all tickets issued 2004-2011)
 #     test.csv - the test set (all tickets issued 2012-2016)
 #     addresses.csv & latlons.csv - mapping from ticket id to addresses, and from addresses to lat/lon coordinates. 
-#      Note: misspelled addresses may be incorrectly geolocated.
-
-# 
-
-# 
-# 
-# ___
-# 
-# ## Evaluation
-# 
-# Your predictions will be given as the probability that the corresponding blight ticket will be paid on time.
-# 
-# The evaluation metric for this assignment is the Area Under the ROC Curve (AUC). 
-# 
-# Your grade will be based on the AUC score computed for your classifier. A model which with an AUROC of 0.7 passes this assignment, over 0.75 will recieve full points.
-# ___
-# 
-# For this assignment, create a function that trains a model to predict blight ticket compliance in Detroit using `train.csv`. Using this model, return a series of length 61001 with the data being the probability that each corresponding ticket from `test.csv` will be paid, and the index being the ticket_id.
 
 
 import pandas as pd
